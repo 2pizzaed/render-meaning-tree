cc7296264aa29e0c05cac734b1e461d902f8e6d5
samedit66 <samedit66@yandex.ru>
2025-04-03 22:46:09 +0300

    feat(jinja): подключил пробную генерацию шаблонов для точки входа в программу

--- main.py
+++ main.py
@@ -1,12 +1,68 @@
+from jinja2 import Environment, FileSystemLoader
+
 from renderer import Renderer
 
 
+environment = Environment(
+    loader=FileSystemLoader("templates/")
+)
 r = Renderer()
 
 
-r.node(type="program_entry_point")
+@r.node(type="program_entry_point")
 def program_entry_point(node):
-    ...
+    template = environment.get_template("base.html")
+    return template.render()
 
 
-
+if __name__ == '__main__':
+    print(r.render(
+        {
+        "type": "program_entry_point",
+        "body": [
+            {
+                "type": "if_statement",
+                "branches": [
+                    {
+                        "condition": {
+                            "type": "lt_operator",
+                            "left_operand": {
+                                "type": "identifier",
+                                "name": "a"
+                            },
+                            "right_operand": {
+                                "type": "int_literal",
+                                "value": 3,
+                                "repr": "DECIMAL"
+                            }
+                        },
+                        "body": {
+                            "type": "compound_statement",
+                            "statements": [
+                                {
+                                    "type": "assignment_statement",
+                                    "target": {
+                                        "type": "identifier",
+                                        "name": "b"
+                                    },
+                                    "value": {
+                                        "type": "add_operator",
+                                        "left_operand": {
+                                            "type": "identifier",
+                                            "name": "b"
+                                        },
+                                        "right_operand": {
+                                            "type": "int_literal",
+                                            "value": 1,
+                                            "repr": "DECIMAL"
+                                        }
+                                    }
+                                }
+                            ]
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+    ))

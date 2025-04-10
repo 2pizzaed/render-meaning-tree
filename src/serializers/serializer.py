71fffb6dc7364aeea0e7b8123838ba08189302b4
bardoor <dmitrybartenev58@gmail.com>
2025-04-11 01:31:36 +0300

    feat: add Serializer class

---
+++ src/serializers/serializer.py
@@ -0,0 +1,26 @@
+from typing import Dict, Any
+
+from src.types import Node, NodeType
+
+
+class SerializerNotFoundError(Exception):
+    pass
+
+
+class Serializer:
+    def __init__(self) -> None:
+        self.serizlize_funcs: Dict[NodeType, Any] = {}
+    
+    def node(self, *, type: NodeType):
+        def decorator(func):
+            self.serizlize_funcs[type] = func
+            return func
+
+        return decorator
+    
+    def serialize(self, node: Node) -> Any:
+        node_type = node.get("type")
+        if node_type in self.serizlize_funcs:
+            return self.serizlize_funcs[node_type](node)
+        
+        raise SerializerNotFoundError(f"No serializer function found for '{node_type}'")

b40b739984a05b2abeaa33488fb9fd4ed55945f7
den1s0v <hello-lumen@ya.ru>
2025-10-05 05:08:40 +0300

    doc: add TODOs

--- src/common_utils.py
+++ src/common_utils.py
@@ -3,14 +3,25 @@ from typing import Any, Self
 
 
 class DictLikeDataclass:
-    """ Mixins for dataclasses to be used as dict """
+    """ Mixins for dataclasses to be used as dict.
+    Also, automatic construction from dict using types declared for property annotations/type hints (mandatory or optional as typing.Optional) is provided (TODO).
+    """
     __getitem__ = getattr
     __setitem__ = object.__setattr__
+    __delitem__ = object.__delattr__
     get = getattr
     __contains__ = hasattr
 
     def make(self, data: dict):
-        """ Try to create instance from dict, creating children recursively using types declared for property annotations/type hints (mandatory or optional as typing.Optional) """
+        """ Try to create instance from dict, creating children recursively using types declared for property annotations/type hints (mandatory or optional as typing.Optional).
+        Behaviour:
+        - if unknown keys are present in data, raise ValueError as no such attributes are expected
+        - if attr is mandatory, raise ValueError if not present in data
+        - if attr is optional, ignore if not present in data
+        - if attr is list, create list of children recursively using types declared for property annotations/type hints (mandatory or optional as typing.Optional)
+        - if attr is dict, create dict of children recursively using types declared for property annotations/type hints (mandatory or optional as typing.Optional)
+        - if attr is Enum, use SelfValidatedEnum.lookup to create instance
+        """
         # TODO: implement for all cases of attr definition
         for key, value in data.items():
             if isinstance(value, dict):

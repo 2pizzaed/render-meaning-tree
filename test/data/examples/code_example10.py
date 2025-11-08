c8647ac15fbaf57a1bd9a5570242db66fc7e38e6
den1s0v <hello-lumen@ya.ru>
2025-11-08 16:05:41 +0300

    chore: annotate code example with ids

--- test/data/examples/code_example10.py
+++ test/data/examples/code_example10.py
@@ -1,8 +1,8 @@
-def g(_):
-    return 'gone'
+def g(_):  # id10 (function_definition) / id9 (function_declaration)
+    return 'gone'  # id7 (return_statement) -> expr id6
 
-if g('go'):
-    a = 'b'
-    d = 'pass'
-    e = 'step == (0,0,0)'
+if g('go'):  # id31 (if_statement) -> call id13 (arg id12)
+    a = 'b'  # id17 (variable_declaration) -> init id15
+    d = 'pass'  # id22 (variable_declaration) -> init id20
+    e = 'step == (0,0,0)'  # id27 (variable_declaration) -> init id25
 

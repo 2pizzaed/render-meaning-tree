b38af6d3c1f62dc6651b4800f23039d6bc39c581
Dmitry Shashkov (Brookit) <brookit18@gmail.com>
2025-12-01 17:12:33 +0300

    Fakegen reports processing files

--- fakegen.sh
+++ fakegen.sh
@@ -1,6 +1,11 @@
 mkdir -p test/output/questions/
 for f in test/data/task_code/*.py; do
     base=$(basename "$f" .py)
+    echo
+    echo "----------------------------------------"
+    echo "Processing $f -> test/output/questions/${base}.json"
+    echo "----------------------------------------"
+    echo
     python generator.py "$f" > "test/output/questions/${base}.json"
 done
 # Useful for debugging:

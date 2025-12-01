79d18c4eeb493a0b91645f155490ce78480d6e86
Dmitry Shashkov (Brookit) <brookit18@gmail.com>
2025-12-01 16:28:33 +0300

    Fix factorial example

--- test/data/task_code/8_factorial.py
+++ test/data/task_code/8_factorial.py
@@ -1,9 +1,13 @@
-def fact(n):
+def fib_dyn(n):
     res = 1
     for i in range(2, n + 1):
         res *= i
     return res
 
+def fact(n):
+    if n <= 1:
+        return 1
+    return n * fact(n - 1)
 
 y = fact(5)
 print("fact(5) =", y)

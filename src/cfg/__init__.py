b22fbbcdb156f43a01bc2f09643a284052300ef9
Dmitry Shashkov (Brookit) <brookit18@gmail.com>
2025-12-07 20:17:38 +0300

    Style improvements

--- src/cfg/__init__.py
+++ src/cfg/__init__.py
@@ -1,4 +1,4 @@
 from src.cfg.abstractions import ConstructSpec
 from src.cfg.ast_wrapper import ASTNodeWrapper
-from src.cfg.cfg_builder import CFGBuilder
 from src.cfg.cfg import CFG, Edge, Node
+from src.cfg.cfg_builder import CFGBuilder

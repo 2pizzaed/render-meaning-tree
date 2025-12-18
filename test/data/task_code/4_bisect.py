665ceca52a3a07c4c6dc06119565c474f89a40cb
den1s0v <hello-lumen@ya.ru>
2025-12-18 07:14:19 +0300

    chore: add scenarios

---
+++ test/data/task_code/4_bisect.py
@@ -0,0 +1,12 @@
+def guess_number(low, high):  
+    guess = (low + high) // 2 # Предположение — середина диапазона
+    status = check_number(guess)
+    if status == '=':
+        return guess  # Найдено
+    elif status == '>':
+        low = guess + 1 # Сужаем диапазон поиска вверх
+    elif status == '<':
+        high = guess - 1 # Сужаем диапазон поиска вниз
+    return guess_number(low, high)
+
+guess_number(0, 10)

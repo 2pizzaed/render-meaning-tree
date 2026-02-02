e41f83dfecfa9deb7fd8d9d8b653abf3f5161fe0
Dmitry Shashkov (Brookit) <brookit18@gmail.com>
2026-02-02 13:08:57 +0300

    Fix task code

--- test/data/task_code/ArrayArrayContainsExactly.py
+++ test/data/task_code/ArrayArrayContainsExactly.py
@@ -5,7 +5,7 @@ check_count = 4
 for i in range(len(nums) - 1):
     check = nums[i] == check_count
     if check:
-        while nums[j] != 5 or (j > 0 and nums[j - 1] == 4):
+        while (nums[j] != 5 or (j > 0 and nums[j - 1] == 4)):
             j += 1
         nums[j], nums[i + 1] = nums[i + 1], nums[j]
 result = nums

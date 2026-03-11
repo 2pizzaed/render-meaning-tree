bac81c1d0ff20a22f5fe4676cb1ac5e762a46940
Dmitry Shashkov (Brookit) <brookit18@gmail.com>
2026-03-11 23:08:44 +0300

    feat(function, html): Step-into/out button only for defined functions

--- src/coderenderer/injections.py
+++ src/coderenderer/injections.py
@@ -1,4 +1,6 @@
 # pyright: reportGeneralTypeIssues=false
+from matplotlib.artist import get
+
 from src.ast_managers import (
     InjectionPoint,
     InjectionPool,
@@ -37,6 +39,7 @@ def is_compound_type(node: NodePathElement) -> bool:
     return node.type.lower() in COMPOUND_STATEMENT_TYPES
 
 
+
 class ControlFlowButtons(InjectionPool):
     @observable_token()
     def is_simple_statement(cursor: TokenCursor):
@@ -73,15 +76,28 @@ class ControlFlowButtons(InjectionPool):
             else None
         )
 
-
     @observable_token()
+    def is_defined_function(cursor: TokenCursor):
+        node = stream_require(cursor.ast_node(0))
+        target = node.find_first(lambda p: p.instanceof("function_call"))
+        if not target:
+            return False
+        if target.instanceof("method_call"):
+            return True
+
+        node_source = target.get(cursor.manager.ast)
+        name = node_source.get("function", {}).get("name")  # type: ignore
+        return node_source and \
+            name in cursor.manager.user_defined_function_names # type: ignore
+
+
+    @observable_token(before=[is_defined_function])
     def is_function_call_start(cursor: TokenCursor):
         node = stream_require(cursor.ast_node(0))
         token = cursor[0]
 
         target = node.find_first(
-                lambda p : p.instanceof("function_call")
-        )
+                lambda p : p.instanceof("function_call"))
 
         return (
             target
@@ -94,7 +110,7 @@ class ControlFlowButtons(InjectionPool):
             else None
         )
 
-    @observable_token()
+    @observable_token(before=[is_defined_function])
     def is_function_call_end(cursor: TokenCursor):
         node = stream_require(cursor.ast_node(0))
         token = cursor[0]
